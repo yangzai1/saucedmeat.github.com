c5a581b3b87425b02e8799a3c3160db6374fc89b
saucedmeat <917164087@qq.com>
2016-12-05 11:18:40 +0800

    2

--- Resume/dsyd/js/main.js
+++ Resume/dsyd/js/main.js
@@ -1,16 +1,16 @@
 var tabs_Swiper = new Swiper('.swiper-container', {
     speed: 500,
     onSlideChangeStart: function () {
-        $(".tabs .active").removeClass('active')
-        $(".tabs a").eq(tabs_Swiper.activeIndex).addClass('active')
+        $(".tabs .active").removeClass('active');
+        $(".tabs a").eq(tabs_Swiper.activeIndex).addClass('active');
     }
 })
 $(".tabs a").on('touchstart mousedown', function (e) {
-    e.preventDefault()
-    $(".tabs .active").removeClass('active')
-    $(this).addClass('active')
-    tabs_Swiper.swipeTo($(this).index())
+    e.preventDefault();
+    $(".tabs .active").removeClass('active');
+    $(this).addClass('active');
+    tabs_Swiper.swipeTo($(this).index());
 })
 $(".tabs a").click(function (e) {
-    e.preventDefault()
+    e.preventDefault();
 })
