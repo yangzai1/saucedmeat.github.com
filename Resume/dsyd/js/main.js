48d2f0700fb649144b27e5d330bcfc62ee27b101
saucedmeat <917164087@qq.com>
2016-12-07 11:34:33 +0800

    scar

--- Resume/dsyd/js/main.js
+++ Resume/dsyd/js/main.js
@@ -1,16 +1,19 @@
 var tabs_Swiper = new Swiper('.swiper-container', {
     speed: 500,
     onSlideChangeStart: function () {
+        //当事件触发时
         $(".tabs .active").removeClass('active');
         $(".tabs a").eq(tabs_Swiper.activeIndex).addClass('active');
+        //返回当前活动块的索引,添加active class
     }
-})
+});
 $(".tabs a").on('touchstart mousedown', function (e) {
     e.preventDefault();
     $(".tabs .active").removeClass('active');
     $(this).addClass('active');
     tabs_Swiper.swipeTo($(this).index());
-})
+});
 $(".tabs a").click(function (e) {
     e.preventDefault();
-})
+    //阻止a的默认事件
+});
