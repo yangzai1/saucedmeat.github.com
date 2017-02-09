541b3a2c545ea1b4c0e77bb94adff6234769d5c2
车托帮 <chetuobang@chetuobangdeMacBook-Pro.local>
2017-02-09 10:38:13 +0800

    2.14

--- Resume/party1/js/rem.js
+++
@@ -1,8 +0,0 @@
-(function(doc,win){
-    var rem=20/375*doc.documentElement.clientWidth;
-    doc.documentElement.style.fontSize=rem+'px';
-    win.onresize=function(){
-        var rem=20/375*doc.documentElement.clientWidth;
-        doc.documentElement.style.fontSize=rem+'px';
-    }
-})(document,window);
